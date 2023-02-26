12f0b7e487c0a94fa8ae233ef658b05588f057d7
Kim Nhai <nhailtk@gmail.com>
2023-02-26 16:13:18 +0700

    Unit automation framework

---
+++ testcases/com/nopcommerce/user/UserRegister.java
@@ -0,0 +1,33 @@
+package com.nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+
+public class UserRegister extends BaseTest {
+	WebDriver driver;
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+	
+	@Test
+	public void TC_01_Register() {
+		
+	}
+	
+	@Test
+	public void TC_02_Register() {
+		
+	}
+	
+	
+	@AfterClass
+	public void afterClass() {
+
+	}
+
+}
